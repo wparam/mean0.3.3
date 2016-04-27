e4dd8754bd2f02b45ed6f4fa201ec9cd669445a4
wparam@hotmail.com <wparam@hotmail.com>
2016-04-27 23:29:56 +0800

    remove mongo

--- app/mysql_models/index.js
+++ app/mysql_models/index.js
@@ -4,14 +4,13 @@ var Sequelize = require('sequelize'),
     winston = require('winston'),
     glob = require('glob');
 module.exports = function(sequelize){
-    console.log('hit');
-    glob('./*.js', function(er, files){
-        if(er){
-            winston.error(er);        
-        }
-        for(var i=0; i<files.length; i++){
-            winston.log('using file:'+ files[i]);
-            require(files[i])(sequelize);
-        }
-    });
+    // glob('./*.js', function(er, files){
+    //     if(er){
+    //         winston.error(er);        
+    //     }
+    //     for(var i=0; i<files.length; i++){
+    //         winston.log('using file:'+ files[i]);
+    //         require(files[i])(sequelize);
+    //     }
+    // });
 };
--- server.js
+++ server.js
@@ -4,33 +4,33 @@
  */
 var init = require('./config/init')(),
 	config = require('./config/config'),
-	mongoose = require('mongoose'),
+	// mongoose = require('mongoose'),
     Sequelize = require('sequelize'),
-    //winston = require('winston'),
+    winston = require('winston'),
 	chalk = require('chalk');
 
 /** 
  * Main application entry file.
  * Please note that the order of loading is important.
  */ 
-//winston.add(winston.transports.File, {filename: 'myapp.log'});
-//winston.remove(winston.transports.Console);
+winston.add(winston.transports.File, {filename: 'myapp.log'});
+winston.remove(winston.transports.Console);
 
 // Bootstrap db connection
-var db = mongoose.connect(config.db.uri, config.db.options, function(err) {
-	if (err) {
-		console.error(chalk.red('Could not connect to MongoDB!'));
-		console.log(chalk.red(err));
-	}
-});
-require('./app/mongo_models/user.server.model.js');
-require('./app/mongo_models/article.server.model.js');
+// var db = mongoose.connect(config.db.uri, config.db.options, function(err) {
+// 	if (err) {
+// 		console.error(chalk.red('Could not connect to MongoDB!'));
+// 		console.log(chalk.red(err));
+// 	}
+// });
+// require('./app/mongo_models/user.server.model.js');
+// require('./app/mongo_models/article.server.model.js');
 
-mongoose.connection.on('error', function(err) {
-	console.error(chalk.red('MongoDB connection error: ' + err));
-	process.exit(-1);
-	}
-);
+// mongoose.connection.on('error', function(err) {
+// 	console.error(chalk.red('MongoDB connection error: ' + err));
+// 	process.exit(-1);
+// 	}
+// );
 
 var sequelize = new Sequelize('mydb', 'acdev', 'acdev', {
     host: 'localhost',
