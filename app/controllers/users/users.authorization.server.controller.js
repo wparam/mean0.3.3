d5be826a42c80e6d7b45c99d0c38473e4c414b85
wparam@hotmail.com <wparam@hotmail.com>
2016-05-29 21:20:33 +0800

    up authorize

--- app/controllers/users/users.authorization.server.controller.js
+++ app/controllers/users/users.authorization.server.controller.js
@@ -4,8 +4,7 @@
  * Module dependencies.
  */
 var _ = require('lodash');
-	// mongoose = require('mongoose'),
-	// User = mongoose.model('User');
+	User = require('../../models').User;
 
 /**
  * User middleware
