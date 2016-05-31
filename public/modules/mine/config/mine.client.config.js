637ad310d1b34728d455abf0fb80a29135708320
wparam@hotmail.com <wparam@hotmail.com>
2016-05-31 22:59:01 +0800

    add a router of mine

--- public/modules/mine/config/mine.client.config.js
+++ public/modules/mine/config/mine.client.config.js
@@ -1,9 +1,16 @@
 'use strict';
 
 angular.module('mine')
-.config(['$stateProvider'], function($stateProvider){
+.config(['$stateProvider', function($stateProvider){
     $stateProvider.state('mine', {
         url: '/mine',
         templateUrl: 'modules/mine/views/mine.client.view.html'
     });
-});
+}]).run(['Menus',
+	function(Menus) {
+		// Set top bar menu items
+		Menus.addMenuItem('topbar', 'Mine', 'mine', 'dropdown', '/mine(/create)?');
+		Menus.addSubMenuItem('topbar', 'mine', 'List mine', 'mine');
+		Menus.addSubMenuItem('topbar', 'mine', 'New mine', 'mine/create');
+	}
+]);
