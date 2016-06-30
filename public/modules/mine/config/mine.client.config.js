a1a4444c17a70d7fe831c5530e7bef0838c2e709
wparam@hotmail.com <wparam@hotmail.com>
2016-06-30 22:05:52 +0800

    up router

--- public/modules/mine/config/mine.client.config.js
+++ public/modules/mine/config/mine.client.config.js
@@ -6,6 +6,14 @@ angular.module('mine')
         url: '/mine',
         templateUrl: 'modules/mine/views/mine.client.view.html'
     })
+	.state('mine-sub1',{
+		url: '/mine/sub1',
+        templateUrl: 'modules/mine/views/mine-sub1.client.view.html'
+	})
+	.state('mine-sub2',{
+		url: '/mine/sub2',
+        templateUrl: 'modules/mine/views/mine-sub2.client.view.html'
+	})
 	.state('minenew',{
 		url:'/mine/create',
 		templateUrl: 'modules/mine/views/minecreate.client.view.html'
